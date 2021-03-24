9b07a3832959661d475967f5cff06489c1781bfb
neokii <neokii@gmail.com>
2021-03-24 16:03:00 +0900

    tune

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -101,7 +101,7 @@ class LongitudinalMpc():
 
     # scc smoother
     cruise_gap = int(clip(CS.cruiseGap, 1., 4.))
-    TR = interp(float(cruise_gap), [1., 2., 3., 4.], [1.0, 1.3, 1.7, 2.2])
+    TR = interp(float(cruise_gap), [1., 2., 3., 4.], [1.2, 1.5, 1.8, 2.2])
     #TR = interp(v_ego, [3., 30.], [1., 2.5])
 
     if self.cruise_gap != cruise_gap:
